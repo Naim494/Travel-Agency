4c92d713e9f251b4352ee56aacf50adfb7c6c8bd
Naim <naimtraore@gmail.com>
2018-05-06 20:36:32 -0400

    logout bug fix

--- app.js
+++ app.js
@@ -64,6 +64,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(80, () => console.log('Travel Agency listening on port 80!'))
+app.listen(3000, () => console.log('Travel Agency listening on port 80!'))
 
 module.exports = app;
